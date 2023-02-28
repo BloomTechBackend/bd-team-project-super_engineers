2bf56cdf94af1b3b3ce5c7b36e8b7d97655dc8b1
Ralph2 <academy4@talentamp.com>
2023-02-28 16:30:14 +0800

    progressWithClasses

--- src/main/java/dental/appointment/clinic/dynamodb/models/Dentist.java
+++ src/main/java/dental/appointment/clinic/dynamodb/models/Dentist.java
@@ -9,8 +9,39 @@ public class Dentist {
     private LocalDateTime availability;
     private List<Appointment> appointmentList;
 
+    public String getName() {
+        return name;
+    }
 
+    public Dentist setName(String name) {
+        this.name = name;
+        return this;
+    }
 
+    public String getContactInformation() {
+        return contactInformation;
+    }
 
+    public Dentist setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+        return this;
+    }
 
+    public LocalDateTime getAvailability() {
+        return availability;
+    }
+
+    public Dentist setAvailability(LocalDateTime availability) {
+        this.availability = availability;
+        return this;
+    }
+
+    public List<Appointment> getAppointmentList() {
+        return appointmentList;
+    }
+
+    public Dentist setAppointmentList(List<Appointment> appointmentList) {
+        this.appointmentList = appointmentList;
+        return this;
+    }
 }
--- src/main/java/dental/appointment/clinic/dynamodb/models/Patient.java
+++ src/main/java/dental/appointment/clinic/dynamodb/models/Patient.java
@@ -1,4 +1,84 @@
 package dental.appointment.clinic.dynamodb.models;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@DynamoDBTable(tableName = "patients")
 public class Patient {
+
+    @DynamoDBHashKey(attributeName = "id")
+    private UUID id = UUID.randomUUID();
+
+    @DynamoDBAttribute(attributeName = "name")
+    private String name;
+
+    @DynamoDBAttribute(attributeName = "contactInformation")
+    private String contactInformation;
+
+    @DynamoDBAttribute(attributeName = "address")
+    private String address;
+
+    @DynamoDBAttribute(attributeName = "appointments")
+    private List<Appointment> appointments;
+
+    public Patient() {}
+
+    public Patient(String name, String contactInformation, String address) {
+        this.name = name;
+        this.contactInformation = contactInformation;
+        this.address = address;
+    }
+
+    @DynamoDBHashKey(attributeName = "id")
+    public UUID getId() {
+        return id;
+    }
+
+    public Patient setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    @DynamoDBAttribute(attributeName = "name")
+    public String getName() {
+        return name;
+    }
+
+    public Patient setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @DynamoDBAttribute(attributeName = "contactInformation")
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public Patient setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+        return this;
+    }
+
+    @DynamoDBAttribute(attributeName = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    public Patient setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    @DynamoDBAttribute(attributeName = "appointments")
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public Patient setAppointments(List<Appointment> appointments) {
+        this.appointments = appointments;
+        return this;
+    }
 }
